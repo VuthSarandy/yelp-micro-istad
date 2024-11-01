0a4c8c8e9c4bbddbacf814693fe0ed906a8544c6
VuthSarandy <dysart9999@gmail.com>
2024-11-01 15:54:20 +0700

    first commit

---
+++ yelp-web-bff/src/main/java/kh/edu/cstad/webbff/YelpWebBffApplication.java
@@ -0,0 +1,13 @@
+package kh.edu.cstad.webbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpWebBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpWebBffApplication.class, args);
+	}
+
+}
---
+++ yelp-web-bff/src/test/java/kh/edu/cstad/webbff/YelpWebBffApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.edu.cstad.webbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelpWebBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
